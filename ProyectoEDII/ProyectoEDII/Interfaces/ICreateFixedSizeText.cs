bb9e6650797cd11ad4984eeabb15c880bd6f8a46
Renato <renatojose98@yahoo.com>
2020-05-04 19:25:52 -0600

    Arbol B* e interfaces

---
+++ ProyectoEDII/ProyectoEDII/Interfaces/ICreateFixedSizeText.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoEDII.Interfaces
+{
+	public interface ICreateFixedSizeText<T> where T : IFixedSizeText
+	{
+		T Create(string FixedSizeText);
+		T CreateNull();
+	}
+}
